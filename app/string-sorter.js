b47d2ee693b2fad245ab8e050bebee2a44081978
Daria <daria.shtanko@gmail.com>
2020-03-22 16:01:41 +0100

    string-sorter

--- app/string-sorter.js
+++ app/string-sorter.js
@@ -7,9 +7,22 @@ const stringSorter = {
   },
   setActive: function(newActive) {
     // set the new active array
+    this.state.active = newActive;
   },
   addString: function(newString) {
     // add the new string to the correct array
+    //const userInput = document.getElementById("input").value;
+    if (isNaN(newString)) {
+      if (!this.state.nany.includes(newString)) {
+        this.state.nany.push(newString);
+      }
+    } else if (Number(newString) % 2 === 1) {
+      if (!this.state.odds.includes(newString)) {
+        this.state.odds.push(newString);
+      }
+    } else if (!this.state.evens.includes(newString)) {
+      this.state.evens.push(newString);
+    }
   },
   renderActiveList: function() {
     // return the active array rendered to a list
