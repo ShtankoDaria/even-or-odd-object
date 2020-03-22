18bca219e64d303fa3d6a881520128eb3e53c2df
Daria <daria.shtanko@gmail.com>
2020-03-22 15:45:22 +0100

    reset

--- app/string-sorter.js
+++ app/string-sorter.js
@@ -1,17 +1,17 @@
 const stringSorter = {
   state: {
-    active: 'evens',
-    evens: ['2', '4'],
-    odds: ['1', '3'],
-    nany: ['x', 'y'],
+    active: "evens",
+    evens: ["2", "4"],
+    odds: ["1", "3"],
+    nany: ["x", "y"]
   },
-  setActive: function (newActive) {
+  setActive: function(newActive) {
     // set the new active array
   },
-  addString: function (newString) {
+  addString: function(newString) {
     // add the new string to the correct array
   },
-  renderActiveList: function () {
+  renderActiveList: function() {
     // return the active array rendered to a list
   }
-}
+};
